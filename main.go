ae3d23b9bfebec13cfe85e13143274ad10af5749
Ayush Kumar <57685648+ha-wk@users.noreply.github.com>
2023-02-20 16:26:50 +0530

    code updated with explanation comments...

--- main.go
+++ main.go
@@ -14,26 +14,26 @@ import (
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title"`
+	Title    string    `json:"title"` //MOVIES ATTRIBUTES
 	Director *Director `json:"director"`
 }
 type Director struct {
-	FirstName string `json:"firstname"`
+	FirstName string `json:"firstname"` //MOVIES ATTRIBUTES
 	LastName  string `json:"lastname"`
 }
 
-var movies []Movie
+var movies []Movie //DATA STRUCTURE TO HOLD MOVIES AND ITS DETAILS
 
 func getMovies(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json") //TO FETCH ALL MOVIES(READ)
 	json.NewEncoder(w).Encode(movies)
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
+	params := mux.Vars(r) //TO DELETE A MOVIE(DELETE)
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
@@ -49,7 +49,7 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, item := range movies {
 
-		if item.ID == params["id"] {
+		if item.ID == params["id"] { //TO GET A SINGLE MOVIE(GET)
 			json.NewEncoder(w).Encode(item)
 			return
 		}
@@ -60,7 +60,7 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
-	_ = json.NewDecoder(r.Body).Decode(&movie)
+	_ = json.NewDecoder(r.Body).Decode(&movie) //TO CREATE A NEW MOVIE(CREATE)
 	movie.ID = strconv.Itoa(rand.Intn(999999))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
@@ -75,7 +75,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 
 	for index, item := range movies {
 		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
+			movies = append(movies[:index], movies[index+1:]...) //TO UPDATE ANY MOVIE ATTRIBUTES(UPDATE)
 			var movie Movie
 			_ = json.NewDecoder(r.Body).Decode(&movie)
 			movie.ID = params["id"]
@@ -89,15 +89,18 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
+	//SAMPLE DUMMIE MOVIE DATA....WE CAN ALSO USE ANY DATABASE FOR SAME!
 	movies = append(movies, Movie{ID: "1", Isbn: "222222", Title: "Movie 1", Director: &Director{FirstName: "John", LastName: "Wick"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "444444", Title: "Movie 2", Director: &Director{FirstName: "Larry", LastName: "Brin"}})
 
+	//ALL REQUIRED ENDPOINTS
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
+	//ASSIGNING PORT WHERE OUR SERVER WILL RUN....
 	fmt.Printf("staring server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
 
